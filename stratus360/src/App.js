bb1bcb8ee5061655a00904781964d8f6cd45946e
Pranav Panchal <panchaldpranav@gmail.com>
2020-02-01 22:29:13 -0500

    Add react-bootstrap

--- stratus360/src/App.js
+++ stratus360/src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs lg="2">
+            1 of 3
+          </Col>
+          <Col md="auto">Variable width content</Col>
+          <Col xs lg="2">
+            3 of 3
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
